8a5840a710c68ce81112ebfe8bbdd02bc4a7a53a
Kfir Levy <kfir258levy@gmail.com>
2020-01-28 21:33:52 +0200

    :(

--- src/app/pre-game/pre-game.component.ts
+++ src/app/pre-game/pre-game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-pre-game',
@@ -15,7 +16,8 @@ export class PreGameComponent implements OnInit {
   readTeams: Array<string> = [];
 
   constructor(private fb: FormBuilder,
-              private http: HttpClient) {
+              private http: HttpClient,
+              private db: AngularFirestore) {
     this.qualNumberForm = fb.group({
       qualNumber: ['', [Validators.required]]
     });
@@ -23,6 +25,32 @@ export class PreGameComponent implements OnInit {
 
   ngOnInit() {
     this.eventKey = localStorage.getItem('event_key');
+  }
+  getTeamAverageScore(teamNumber: string) {
+    this.db.collection('tournaments').doc(localStorage.getItem('tournament'))
+      .collection('teams').doc(teamNumber).get().subscribe((res: any) => {
+      console.log(res);
+    });
+    // .valueChanges()
+    // .subscribe((res: any) => {
+    //   if (res) {
+    //     this.pitScoutingSaved = res.pit_scouting_saved;
+    //     if (this.pitScoutingSaved) {
+    //       this.robotLength = res['pit_data']['robot_basic_data']['robot_length'];
+    //       this.robotWeight = res['pit_data']['robot_basic_data']['robot_weight'];
+    //       this.robotWidth = res['pit_data']['robot_basic_data']['robot_width'];
+    //       this.dtMotors = [];
+    //       this.dtMotors.push(['DT Motors', Number(res['pit_data']['robot_basic_data']['dt_motors'])]);
+    //       // this.dtMotors.push(['aaa', 4]);
+    //       console.log(this.dtMotors);
+    //       this.robotDimensions = [];
+    //       this.robotDimensions.push(
+    //         ['Robot Length', res['pit_data']['robot_basic_data']['robot_length']],
+    //         ['Robot Weight', res['pit_data']['robot_basic_data']['robot_weight']],
+    //         ['Robot Width', res['pit_data']['robot_basic_data']['robot_width']],
+    //       );
+    //     }
+    //   }
   }
   qualSelected() {
     this.qualNumber = this.qualNumberForm.getRawValue().qualNumber;
@@ -33,8 +61,12 @@ export class PreGameComponent implements OnInit {
       .subscribe((matchData: any) => {
         this.blueTeams = matchData.alliances.blue.team_keys;
         this.readTeams = matchData.alliances.red.team_keys;
-
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < this.blueTeams.length; i++) {
+          this.readTeams[i] = (this.readTeams[i].substring(3, 10));
+        }
         console.log(this.readTeams);
+        this.getTeamAverageScore(this.readTeams[1]);
       });
     console.log(this.qualNumber);
   }

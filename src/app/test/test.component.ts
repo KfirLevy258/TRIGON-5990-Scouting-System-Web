846c4a78e3b14d260f3e9d018d1faabbcffe3349
Ran Levy <RanLevy62@gmail.com>
2020-02-07 02:06:18 +0200

    draw robot swing on test component

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {ChartDataSets} from 'chart.js';
-import {Color, Label} from 'ng2-charts';
+import {Component, ElementRef, OnInit, ViewChild,} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+
 
 @Component({
   selector: 'app-test',
@@ -8,38 +8,99 @@ import {Color, Label} from 'ng2-charts';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+  res: number;
+  @ViewChild('canvas', {static: true}) canvas: ElementRef;
+  private cx: CanvasRenderingContext2D;
 
-  lineChartData: ChartDataSets[] = [
-    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
-    // { data: [72, 78, 75, 77, 75, 82], label: 'Processed oil prices' },
-  ];
-
-  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
-
-  lineChartOptions = {
-    responsive: true,
-    scales: {
-      yAxes: [{
-        ticks: {
-          min: 50,
-        }
-      }]
-    }
-  };
-
-  lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,255,0,0.28)',
-    },
-  ];
-
-  lineChartLegend = true;
-  lineChartPlugins = [];
-  lineChartType = 'line';
+  constructor(private fb: FormBuilder) {
+  }
+  testForm: any;
 
   ngOnInit() {
+    this.testForm = this.fb.group({
+      m1: [''],
+      d1: [''],
+      m2: [''],
+      d2: [''],
+      m3: [''],
+      d3: [''],
+    });
 
+    this.cx = this.canvas.nativeElement.getContext('2d');
+    console.log(this.cx);
   }
 
+  calc() {
+    const PIVOT_TO_RUNG = 48 * 2.54 * 0.01; // [m]
+    const MASS = 93 * 0.453592; // [Kg]
+    const CENTER_OF_MASS_OFFSET = 26 * 2.54 * 0.01; // [m]
+    let  m1: number; let d1: number;
+    let  m2: number; let d2: number;
+    let  m3: number; let d3: number;
+
+    m1 = Number(this.testForm.value.m1);
+    d1 = Number(this.testForm.value.d1);
+    m2 = Number(this.testForm.value.m2);
+    d2 = Number(this.testForm.value.d2);
+    m3 = Number(this.testForm.value.m3);
+    d3 = Number(this.testForm.value.d3);
+
+    console.log(d1);
+
+    console.log(m1 * d1 + m2 * d2 + m3 * d3);
+    console.log((m1 + m2 + m3) * 48 * 2.54 * 0.01  + 93 * 0.453592 * 26 * 2.54 * 0.01);
+    this.res = Math.atan((m1 * d1 + m2 * d2 + m3 * d3) / ((m1 + m2 + m3) * PIVOT_TO_RUNG  + MASS * CENTER_OF_MASS_OFFSET)) ;
+    this.draw(this.res, d1, d2, d3);
+  }
+
+  draw(angle: number, d1: number, d2: number, d3: number) {
+    console.log(this.canvas.nativeElement.width);
+    if (d2 === d1) {d2 = d1 + 0.1; }
+    if (d3 === d1) {d3 = d1 + 0.1; }
+    if (d3 === d2) {d3 = d2 + 0.1; }
+
+    this.cx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+    this.cx.lineWidth = 3;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+
+    this.cx.beginPath();
+    this.cx.moveTo(200 - 100 * Math.cos(angle), 200 - 100 * Math.sin(angle));
+    this.cx.lineTo(200 + 100 * Math.cos(angle), 200 + 100 * Math.sin(angle));
+    this.cx.stroke();
+
+    this.cx.beginPath();
+    this.cx.arc(200 + d1 / 1.4 * 100 * Math.cos(angle), 200 + d1 / 1.4 * 100 * Math.sin(angle),
+      10, 0, 2 * Math.PI, false);
+    this.cx.fillStyle = 'green';
+    this.cx.fill();
+    this.cx.lineWidth = 1;
+    this.cx.strokeStyle = '#003300';
+    this.cx.stroke();
+
+    this.cx.beginPath();
+    this.cx.arc(200 + d2 / 1.4 * 100 * Math.cos(angle), 200 + d2 / 1.4 * 100 * Math.sin(angle),
+      10, 0, 2 * Math.PI, false);
+    this.cx.fillStyle = 'red';
+    this.cx.fill();
+    this.cx.lineWidth = 1;
+    this.cx.strokeStyle = '#003300';
+    this.cx.stroke();
+
+    this.cx.beginPath();
+    this.cx.arc(200 + d3 / 1.4 * 100 * Math.cos(angle), 200 + d3 / 1.4 * 100 * Math.sin(angle),
+      10, 0, 2 * Math.PI, false);
+    this.cx.fillStyle = 'blue';
+    this.cx.fill();
+    this.cx.lineWidth = 1;
+    this.cx.strokeStyle = '#003300';
+    this.cx.stroke();
+  }
+
+  clear() {
+    console.log('clear');
+    this.cx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+  }
 }

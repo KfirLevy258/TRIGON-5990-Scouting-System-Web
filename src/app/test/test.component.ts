344b96a96fcce567f3e54e5ee0b322f9e728aeb5
Ran Levy <RanLevy62@gmail.com>
2020-02-25 04:46:49 +0200

    test component

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {take} from 'rxjs/operators';
 
@@ -10,21 +10,39 @@ import {take} from 'rxjs/operators';
 })
 export class TestComponent implements OnInit {
 
+  @ViewChild('source', {static: false}) sourceRef;
+  @ViewChild('dest', {static: false}) destRef;
+
   constructor(private db: AngularFirestore) {
   }
 
   ngOnInit() {
-    this.db.collection('tournaments').doc('ISRD1').collection('teams').doc('temp').collection('games').get()
-      .pipe(take(1))
-      .subscribe(res => {
-        console.log(res.docs);
-        res.docs.forEach(doc => {
-          console.log(doc.id);
-          this.db.collection('tournaments').doc('ISRD1').collection('teams').doc('1577').collection('games').doc(doc.id).set(doc.data())
-            .catch(err => console.log(err));
+    // this.db.collection('tournaments').doc('ISRD1').collection('teams').doc('temp').collection('games').get()
+    //   .pipe(take(1))
+    //   .subscribe(res => {
+    //     console.log(res.docs);
+    //     res.docs.forEach(doc => {
+    //       console.log(doc.id);
+    //       this.db.collection('tournaments').doc('ISRD1').collection('teams').doc('1577').collection('games').doc(doc.id).set(doc.data())
+    //         .catch(err => console.log(err));
+    //
+    //     });
+    //   });
+  }
 
+  start() {
+    // const source = this.sourceRef.nativeElement.value;
+    const source = 'ISRD1';
+    // const dest = this.destRef.nativeElement.value;
+    const dest = 'temp';
+
+    this.db.collection('tournaments').doc(source).collection('teams').get()
+      .pipe(take(1))
+      .subscribe(sourceTeams => {
+        sourceTeams.docs.forEach(team => {
+          this.db.collection('tournaments').doc(dest).collection('teams').doc(team.id).set(team.data())
+            .catch(err => console.log(err));
         });
       });
   }
-
 }
